1130ce0157cca165e4c49a2dfd5ba3ae3357f7b1
Lee, Dakyum <dlee@lcs-mba3088.lcs.on.ca>
2018-04-12 14:13:43 -0400

    Try 1

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -9,9 +9,37 @@
 import Foundation
 
 // Get the user input
-var rawInput = readLine()
+//var rawInput = readLine()
 
 // Print out the input provided
-print("You said:")
-print(rawInput)
+//print("You said:")
+//print(rawInput)
 
+// number of deficient, perfect, and abundant
+
+var number : Int
+var counter : Int
+var sum : Int
+var range : Int
+
+number = 7
+counter = 1
+sum = 0
+
+for number in range{
+    for counter in range{
+        if (number * counter == number){
+            sum = sum + number
+            sum = sum + counter
+            if sum > number{
+                print("The value", number, "is an abundant number.")
+            } else {sum<number{
+                print("The value", number, "is a deficient number.")
+                break
+                return "perfect"
+                }
+}
+
+}
+}
+}
